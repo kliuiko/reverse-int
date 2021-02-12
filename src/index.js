a489ea0087f9efdcc8e971609661c363fb95b3ef
kliuiko <o.kliuiko@gmail.com>
2021-02-13 01:30:54 +0300

    feature: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+
+    let result = '';
+    let str = String(Math.abs(n));
+
+    for(let i = str.length - 1; 0 <= i; i--){
+
+        result += str[i];
+
+    }
+
+    return Number(result);
+
 }
